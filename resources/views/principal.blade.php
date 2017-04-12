7840cee22a24541f49599d0fc624940f9205a7f8
csimbaUTC <simba199237@gmail.com>
2017-04-12 14:08:29 -0500

    Cuarto comit

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -3,6 +3,51 @@
 <head>
 <title>@yield('title','Default')| Portafolio Docente</title>
 
+
+
+
+
+
+
+
+
+
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @yield('css')
     
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">

ee29da1f86561d470a1910c96426e37128d9f83b
csimbaUTC <simba199237@gmail.com>
2017-04-12 14:10:43 -0500

    Cuarto comit

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -6,48 +6,6 @@
 
 
 
-
-
-
-
-
-
-
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
-
-
-    <!-- Scripts -->
-    <script>
-        window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-        ]); ?>
-    </script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @yield('css')
     
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
@@ -100,7 +58,7 @@ $fotoUser=Auth::user()->fotos;
     <div class="content-wrapper" style="min-height:2000px;">
      @include('Pagina.subMenu')
 
-     @include('pagina/menu')
+     @include('Pagina/menu')
    
       @yield('content')
 

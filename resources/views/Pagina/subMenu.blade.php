1a15685105fa64c00632fd122577fe5af9bc314a
csimbaUTC <simba199237@gmail.com>
2017-04-12 19:02:24 -0500

    hacerlo mejor

--- resources/views/Pagina/subMenu.blade.php
+++ resources/views/Pagina/subMenu.blade.php
@@ -1,4 +1,4 @@
-<ol class="breadcrumb" style=" background:red"> 
+<ol class="breadcrumb" style=" background:blue"> 
 <li class="treeview"><a href="{{ url('/home') }}"><i class="fa fa-home"></i> <span>INICIO</span> </a></li>
 <li style="display: none;"  id="portafolios" class="treeview"><a href="javascript:void(0);" onclick="portafolioHome();"><i class="glyphicon glyphicon-briefcase"></i> <span>PORTAFOLIO</span> </a></li>
 <input type="text" name="docente" value="{{ Auth::user()->IdDoc}}" id="docente">

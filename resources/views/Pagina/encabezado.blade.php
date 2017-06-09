3a71b01dfc110ea5b7a26b65dfc5deb80b76868a
csimbaUTC <simba199237@gmail.com>
2017-06-09 14:08:11 -0500

    primerss cambaaioss

--- resources/views/Pagina/encabezado.blade.php
+++ resources/views/Pagina/encabezado.blade.php
@@ -30,9 +30,6 @@
                       <img src="{{url($fotoUser)}}"  class="img-circle"  alt="User Image"  style="width:50px;height:50px;">               
                     <p>
 <!--Asignar rol--> 
-@foreach(Auth::user()-> getNameRole() as $roless)
-{!! $roless."," !!}
-@endforeach
 
                        </b>
 
